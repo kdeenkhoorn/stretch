505d318a475fe06c94efe600074bb38ab7491ac3
Klaas Eenkhoorn <klaas@phantasyworld.intern>
2018-03-27 22:52:16 +0200

    Changed Dockerfile to use Docker hub

--- create_image.sh
+++ create_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo /usr/share/docker-ce/contrib/mkimage.sh -t cubian.phantasyworld.intern:5000/kdedesign/debian-stretch debootstrap --variant=minbase stretch
+sudo /usr/share/docker-ce/contrib/mkimage.sh -t kdedesign/debian-stretch debootstrap --variant=minbase stretch
